100f2f5d75044d1190176a42608aa14e74b4cd0a
Tianyi Ma <tn.ma@outlook.com>
2017-12-29 02:11:47 -0600

    Add port arg

--- src/main/java/com/howmuchismyadaworth/api/HttpApi.java
+++ src/main/java/com/howmuchismyadaworth/api/HttpApi.java
@@ -6,7 +6,12 @@ import spark.Spark;
 public class HttpApi {
 
     public static void main(String[] args) {
+        int port = 4567;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
         new ValueUpdateThread().start();
+        Spark.port(port);
         Spark.get("/", ((request, response) -> "<a href=\"https://howmuchismyadaworth.com/\">https://howmuchismyadaworth.com/</a>"));
         Spark.get("/v1/value/:currency", new GetValueRoute());
     }
